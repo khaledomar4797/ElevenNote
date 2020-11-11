766ec014819951070d4eb1dced0a5b8cdf38df69
Khaled Omar <omark01@pfw.edu>
2020-11-11 16:02:01 -0500

    Added Category Service, Controller, and Model.

---
+++ ElevenNote.Services/CategoryService.cs
@@ -0,0 +1,71 @@
+﻿using ElevenNote.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class CategoryService
+    {
+        public bool CreateCategory(Category model)
+        {
+            var entity = new Category
+            {
+                Name = model.Name
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Categories.Add(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Categories;
+
+                return query.ToArray();
+            }
+        }
+
+        public Category GetCategoryById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Categories.Single(e => e.CategoryId == id);
+                return entity;
+            }
+
+        }
+
+        public bool UpdateCategory(Category model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Categories.Single(e => e.CategoryId == model.CategoryId);
+                        
+                entity.Name = model.Name;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteCategory(int categoryId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Categories.Single(e => e.CategoryId == categoryId);
+
+                ctx.Categories.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
---
+++ ElevenNote.WebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,71 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNote.WebAPI.Controllers
+{
+    [Authorize]
+    public class CategoryController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            CategoryService categoryService = new CategoryService();
+
+            var categories = categoryService.GetCategories();
+
+            return Ok(categories);
+        }
+
+        public IHttpActionResult Post(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            CategoryService categoryService = new CategoryService();
+
+            if (!categoryService.CreateCategory(category))
+            {
+                return InternalServerError();
+            }
+
+            return Ok();
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            CategoryService categoryService = new CategoryService();
+            var category = categoryService.GetCategoryById(id);
+            return Ok(category);
+        }
+
+        public IHttpActionResult Put(Category category)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            CategoryService categoryService = new CategoryService();
+
+            if (!categoryService.UpdateCategory(category))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            CategoryService categoryService = new CategoryService();
+
+            if (!categoryService.DeleteCategory(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
